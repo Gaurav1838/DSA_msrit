4a5ce7c5e3e22da840f4d1142dcc333ef317d309
Gaurav1838 <109055217+Gaurav1838@users.noreply.github.com>
2025-01-08 15:34:41 +0530

    Add files via upload

---
+++ lab/string_operations.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+int top=-1,n;
+
+void push(int a[],int e)
+{
+    if(top==n-1)
+    {
+        printf("stack is ful \n");
+    }
+    else
+    {
+        top++;
+        a[top]=e;
+        printf("*************************\n");
+        printf("\n Element %d is pushed \n",e);
+        printf("*************************\n");
+    }
+}
+
+void pop(int a[])
+{
+    if(top == -1)
+    {
+        printf("************************\n");
+        printf("stack is underflow \n");
+        printf("************************\n");
+    }
+    else
+    {
+        printf("*************************\n");
+        printf("\n Element %d is popped \n",a[top--]);
+        printf("*************************\n");
+    }
+}
+
+void peak(int a[])
+{
+    if(top == -1)
+    {
+        printf("*************************\n");
+        printf("stack is underflow\n");
+        printf("*************************\n");
+    }
+    else
+    {
+        printf("*************************\n");
+        printf("\n The top most element %d is popped \n",a[top]);
+        printf("*************************\n");
+    }
+}
+
+void display(int a[])
+{
+    if (top == -1)
+    {
+        printf("stack is empty\n");
+    }
+    else
+    {
+        printf("elements of stack are \n");
+        printf("******************\n");
+        for(int i=top;i>-1;i--)
+        {
+            printf("%d\n",a[i]);
+        }
+    }
+}
+
+void main()
+{
+    printf("Enter the size of the array \n");
+    scanf("%d",&n);
+    int a[n],e;
+    while(1)
+    {
+        int ch;
+        printf("\n\n1-> insert element \t2->delete element\n3->peak\t\t4->Display\n5->exit\n");
+        printf("Enter choice:\n ");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: pritf("Enter the element to be pushed\n");
+            scanf("%d",&e);
+            push(a,e);
+            break;
+            case 2: pop(a);
+            break;
+            case 3: peep(a);
+            break;
+            case 4: display(a);
+            break;
+            default: printf("enter the choice");
+        }
+    }
+}
