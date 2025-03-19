9441a0145490ccd63f68c55d2651cd9427ece102
Gaurav1838 <109055217+Gaurav1838@users.noreply.github.com>
2025-03-19 21:58:12 +0530

    Update stackUsingLL.c

--- lab/stackUsingLL.c
+++ lab/stackUsingLL.c
@@ -1,86 +1,86 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-struct node{
+// Define a structure for a node in the linked list
+struct node {
     int data;
     struct node *next;
 };
-struct node *top=0;
 
-void push(int x){
-    struct node *newnode = (struct node*)malloc(sizeof(struct node));
-    newnode->data=x;
-    newnode->next=top;
-    top = newnode;
+// Initialize the top of the stack as NULL
+struct node *top = 0;
+
+// Function to push an element onto the stack
+void push(int x) {
+    struct node *newnode = (struct node*)malloc(sizeof(struct node)); // Allocate memory for new node
+    newnode->data = x; // Assign data to the new node
+    newnode->next = top; // Point new node to previous top
+    top = newnode; // Update top to new node
 }
 
-void display(){
-    struct node *temp;
-    temp=top;
-    if(top==0){
-        printf("stack is empty\n");
-    }
-    else{
-        while(temp!=0){
-            printf("%d\n",temp->data);
-            temp=temp->next;
+// Function to display the elements of the stack
+void display() {
+    struct node *temp = top; // Start from the top
+    if (top == 0) {
+        printf("Stack is empty\n");
+    } else {
+        while (temp != 0) { // Traverse through stack
+            printf("%d\n", temp->data);
+            temp = temp->next;
         }
     }
 }
 
-void peek(){
-    if(top==0){
-        printf("stack is empty\n");
-    }
-    else{
-        printf("top element is %d\n",top->data);
+// Function to get the top element of the stack
+void peek() {
+    if (top == 0) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Top element is %d\n", top->data);
     }
 }
 
-void pop(){
+// Function to pop an element from the stack
+void pop() {
     struct node *temp;
-    temp=top;
-     if(top==0){
-        printf("stack is empty\n");
-    }
-    else{
-        printf("the popped element is %d\n",top->data);
-        top=top->next;
-        free(temp);
+    temp = top;
+    if (top == 0) {
+        printf("Stack is empty\n");
+    } else {
+        printf("The popped element is %d\n", top->data);
+        top = top->next; // Move top to next node
+        free(temp); // Free the memory of the popped node
     }
 }
 
-void main(){
+// Main function to run the stack operations
+void main() {
     int ch, n;
-    while (1)
-    {
-    
-    printf("1 Push\n2 Pop\n3 Peep\n4 Display\n5 Exit\n");
-    printf("\nEnter your choice: ");
-    scanf("%d",&ch);
+    while (1) {
+        printf("1 Push\n2 Pop\n3 Peek\n4 Display\n5 Exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
 
-    switch (ch)
-    {
-    case 1:
-        printf("\nEnter data to insert: ");
-        scanf("%d",&n);
-        push(n);
-        break;
-    case 2:
-        pop();
-        break;
-    case 3:
-        peek();
-        break;
-    case 4:
-        display();
-        break;
-    case 5:
-        exit(0);
-
-    default:
-        break;
+        switch (ch) {
+            case 1:
+                printf("\nEnter data to insert: ");
+                scanf("%d", &n);
+                push(n);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("Invalid choice, please try again.\n");
+                break;
+        }
     }
-}
 }
